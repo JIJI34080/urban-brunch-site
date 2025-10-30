29e8c14b40d89dc8d67d00d980df47c69b269d18
JIJI34080 <jab.bi098765@gmail.com>
2025-10-30 01:47:57 +0100

    Delete app/robots.txt/route.ts

--- app/robots.txt/route.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://urban-brunch.example.com';
-  return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: `${baseUrl}/sitemap.xml`
-  };
-}
